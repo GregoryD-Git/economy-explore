ca4a3022394d1412baff5a72f30e282dba816159
Dr. Dan Gregory <d23gregory@gmail.com>
2025-04-04 09:33:59 -0400

    Update to use only CPI-U seasonally adjusted
    Needed to register to get access to high frequency use. Worked at first, but now code won't work for some reason...

--- get_CPI_Udata.py
+++ get_CPI_Udata.py
@@ -5,6 +5,20 @@ import json
 import prettytable
 import pandas as pd
 
+# CUUR0000SA0: This refers to the CPI-U (Consumer Price Index for All Urban 
+# Consumers) for all items in the U.S. city average. The "U" indicates that the 
+# data is not seasonally adjusted.
+
+# For seasonally adjusted CPI data from the BLS, you should use series tags 
+# where the third character is "S" (indicating seasonally adjusted data). 
+# For example: CUUS0000SA0: Seasonally adjusted CPI-U 
+# (Consumer Price Index for All Urban Consumers) for all items in the U.S. 
+# city average.
+
+# SUUR0000SA0: This represents the Chained CPI-U (Consumer Price Index for All 
+# Urban Consumers) for all items in the U.S. city average. The "U" also indicates 
+# that the data is not seasonally adjusted.
+
 # month list
 months = ['January','February','March',
              'April','May','June','July',
@@ -12,7 +26,8 @@ months = ['January','February','March',
              'November','December']
 
 def get_CPI(start_terms, end_terms):
-    rows = []
+    
+    
     for i, start_date in enumerate(start_terms):
         start_year = start_date.split('-')[0]
         # print(f'Start Year: {start_year}')
@@ -20,9 +35,11 @@ def get_CPI(start_terms, end_terms):
         # print(f'End Year: {end_year}')
         
         headers = {'Content-type': 'application/json'}
-        data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":start_year, "endyear":end_year})
+        data = json.dumps({"seriesid": ['CUSR0000SA0'],"startyear":start_year, "endyear":end_year})
         p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
         json_data = json.loads(p.text)
+        rows = []
+        CPI_df = pd.DataFrame()
         
         for series in json_data['Results']['series']:
             x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
@@ -41,14 +58,14 @@ def get_CPI(start_terms, end_terms):
                     x.add_row([seriesId,year,month,value,footnotes[0:-1]])
                     rows.append({'year': year, 'month': month, 'value': value})
             
-            CPI_df = pd.DataFrame(rows)
-            
+                df = pd.DataFrame(rows)
+            CPI_df = pd.concat([CPI_df, df], ignore_index=True, axis=0)
             # Export to a .csv file
             CPI_df.to_csv(f'{seriesId}_CPI_file.csv', index=False)
-    
-            output = open(seriesId + '.txt','w')
-            output.write (x.get_string())
-            output.close()
+
+        # output = open(seriesId + '.txt','w')
+        # output.write (x.get_string())
+        # output.close()
         
     
 
